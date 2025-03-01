25dc804596ee3fdf0af2988966144b9bf84ad37a
sugiyanto <giyanto77@gmail.com>
2025-03-01 07:39:50 +0000

    PWA

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'kutipan-islami-v1';
+const urlsToCache = [
+  '.',
+  'index.html',
+  'css/styles.css',
+  'js/scripts.js',
+  'manifest.json',
+  'https://fonts.googleapis.com/css2?family=Amiri&display=swap'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch resources
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate and clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
